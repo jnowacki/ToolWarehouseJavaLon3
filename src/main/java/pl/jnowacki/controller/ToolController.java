9b9f4532557f9d428b741e964c1c3e2da364de47
Jedrzej Nowacki <contact@jnowacki.pl>
2020-07-05 14:48:59 +0200

    Controller - DONE

--- src/main/java/pl/jnowacki/controller/ToolController.java
+++ src/main/java/pl/jnowacki/controller/ToolController.java
@@ -1,4 +1,17 @@
 package pl.jnowacki.controller;
 
-public class ToolController {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/")
+public class ToolController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
 }
