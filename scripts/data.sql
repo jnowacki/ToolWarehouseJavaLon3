08e1c3dccccfc1bbc6a96e109e8a8d47a265170f
Jedrzej Nowacki <contact@jnowacki.pl>
2020-07-11 14:47:24 +0200

    User in DB and model

--- scripts/data.sql
+++ scripts/data.sql
@@ -1,16 +1,28 @@
 create database javalon3tools;
 use javalon3tools;
 
-CREATE TABLE `tools` (
-                         `id` INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                         `name` VARCHAR(50) NOT NULL,
-                         `type` VARCHAR(50) NOT NULL,
-                         `available` BOOLEAN NOT NULL
+CREATE TABLE `tools`
+(
+    `id`        INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `name`      VARCHAR(50)  NOT NULL,
+    `type`      VARCHAR(50)  NOT NULL,
+    `available` BOOLEAN      NOT NULL
 );
 
-insert into `tools` (`name`, `type`, `available`) values
-('Old saw', 'SAW', 1),
-('Sonic screwdriver', 'SCREWDRIVER', 0),
-('Red hammer', 'HAMMER', 1),
-('Blue hammer', 'HAMMER', 0),
-('Drill', 'OTHER', 1);
+insert into `tools` (`name`, `type`, `available`)
+values ('Old saw', 'SAW', 1),
+       ('Sonic screwdriver', 'SCREWDRIVER', 0),
+       ('Red hammer', 'HAMMER', 1),
+       ('Blue hammer', 'HAMMER', 0),
+       ('Drill', 'OTHER', 1);
+
+CREATE TABLE `users`
+(
+    `id`       INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `login`    VARCHAR(50)  NOT NULL UNIQUE,
+    `password` VARCHAR(250) NOT NULL
+);
+
+insert into `users` (`login`, `password`)
+values ('user1', 'user1'),
+       ('user2', 'user2');
